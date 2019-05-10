48ad6fcb1671b6f48e91e92d2076d3136e6390c5
Joseph Surin <joseph.surin@gmail.com>
2019-05-10 20:27:22 +1000

    add stego challenge

---
+++ src/challs/stego1.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const { getBotConfig, genChallEmbed } = require(path.join(__dirname, '../util/util'))
+const { themecolour } = getBotConfig()
+const { Chall } = require(path.join(__dirname, '../../models/index'))
+
+var chall = {
+	challid: 'stego1',
+	title: 'Darkness',
+	category: 'Steganography',
+	points: 10,
+	author: 'joseph#8210 (Joseph)',
+	flag: 'e01c55e0401ab7eba9911f687b2935107ff1d524fa2da420fc96501fc402362c',
+	desc: async function(msg) {
+		var description = `
+
+			What do you think of my [painting](https://drive.google.com/open?id=1MkfM3miAaUfSxFeKjoQwQR_Y3DOOcenv)?
+		`
+
+		let { challid, title, category, points, author } = chall
+		var d = await Chall.findOne({ challid })
+		var solveCount = d.solves.length
+		var icon_url = 'https://cdn.discordapp.com/avatars/111028987836313600/9a177eb8ca0e33965d894ccc840d3f4b.jpg?size=32'
+
+		var descEmbed = genChallEmbed({
+			challid, title, category, points, author, solveCount, themecolour, description, icon_url
+		})
+
+		msg.channel.send({ embed: descEmbed })
+	}
+}
+
+module.exports = chall
