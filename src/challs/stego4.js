38fbfbed55ff2b6e44cca95381889c3c831f36f1
Joseph Surin <joseph.surin@gmail.com>
2019-06-21 10:12:02 +1000

    add stego4 chall

---
+++ src/challs/stego4.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const { getBotConfig, genChallEmbed } = require(path.join(__dirname, '../util/util'))
+const { themecolour } = getBotConfig()
+
+const description = `[Something happened to my wav file](https://drive.google.com/open?id=1-PJYVQhIIKixV6WWDUTVIGm79v-mbT2N) and now there are screeches all over the place :slight_frown:
+
+It seems so mechanical and exact though... maybe it's the universe trying to tell me something :thinking:`
+
+var chall = {
+    challid: 'stego4',
+    title: 'Kimi no Na wa.',
+    category: 'Steganography',
+    points: 110,
+    authorid: '111028987836313600',
+    authorName: 'Joseph',
+    flag: 'b7b5beb4f0e423567b0dd1347b0d1889e162654ac6faddc4e75e669dbf7711ed',
+    description,
+    desc: async function(msg) {
+
+        let { challid, title, category, points, authorid, authorName, description } = chall
+
+        var descEmbed = await genChallEmbed({
+            challid, title, category, points, authorid, authorName, themecolour, description
+        })
+
+        msg.channel.send({ embed: descEmbed })
+    },
+    notes: ['Use headphones!', 'https://www.sonicvisualiser.org/']
+}
+
+module.exports = chall
